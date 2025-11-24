dbd0680e6116d833895d64aa8c9493ae355aa7c0
ManonChab <godfroymanon@gmail.com>
2025-11-24 11:47:01 +0100

    arrays de objectos de patitos

---
+++ js/catalogo.js
@@ -0,0 +1,26 @@
+let listaPatitos = [
+    {nombre: "Patito Estrella Negro",
+     estrellas: 5, 
+     precio: 15, 
+     moneda: "€",
+     rol: "Para problemas de HTML"},
+    {nombre: "Patito Mago",
+     estrellas: 2, 
+     precio: 16,
+     moneda: "€",
+     rol: "Para problemas de CSS "},
+    {nombre: "Patito Estrella Azul", 
+     estrellas: 3, 
+     precio: 15, 
+     moneda: "€",
+     rol: "Para problemas de Javascript"},
+    { nombre: "Patito Harry", 
+     estrellas: 5, 
+     precio: 20, 
+     moneda: "€",
+     rol: "Para problemas de Python"}
+]
+
+function crearCuadro () {
+    
+}
