436761e1902e0181e05af4e67e70e612ea18ea47
majoz-t <majozta@gmail.com>
2025-11-26 12:16:51 +0100

    Update contador.js

--- js/contador.js
+++ js/contador.js
@@ -1,17 +1,35 @@
+import { listaPatitos } from "./lista-patitos.js";
+import {duck} from"./producto.js"
+
 let contador= 0;
 
 const botonmenos= document.getElementById("botondisminuir");
 const botonmas= document.getElementById("botonincrementar");
 const quantity= document.getElementById("contador")
+console.log(duck)
+
+let nstock= duck.stock;
+
+if (nstock) {
+    console.log(nstock) 
+} else {
+    alert("Sin stock")
+}
 
 botonmas.addEventListener("click", () => {
+    if(contador < nstock){
     contador ++;
     quantity.textContent=contador
+   } else{
+    alert("Sin stock");
+   }
 })
 
 botonmenos.addEventListener("click",() => {
     if (contador>0) {
         contador--;
         quantity.textContent=contador
+    } else{
+        alert("Ingrese una cantitad");
     }
 })
